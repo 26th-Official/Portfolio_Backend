8afe6d242bec449649f5fb595c1f8a68a9dd8f3a
26th-Official <you@example.com>
2023-12-01 13:26:18 +0530

    updated

---
+++ TODO.md
@@ -0,0 +1,16 @@
+# Project
+
+Project Description
+
+<em>[TODO.md spec & Kanban Board](https://bit.ly/3fCwKfM)</em>
+
+### Todo
+
+- [ ] Remove the fork and make it as a fork for - "Portfolio - Backend" repo  
+
+### In Progress
+
+
+### Done ✓
+
+
--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
+const axios = require('axios');
+
 
 require('dotenv').config();
 
@@ -15,10 +17,34 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.json({
-    message: '🦄🌈✨👋🌎🌍🌏✨🌈🦄',
-  });
+app.get('/', async (req, res) => {
+  const { username } = req.query;
+
+  if (!username) {
+    return res.status(400).json({ error: 'Username parameter is missing.' });
+  }
+
+  const url = `https://www.artstation.com/users/${username}/projects.json`;
+
+  const headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/117.0',
+    "Accept": 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+    "Accept-Language": 'en-US,en;q=0.5',
+    "Connection": 'keep-alive',
+}
+
+  try {
+    const response = await axios.get(url, { headers : headers });
+
+    if (response.status === 200) {
+      const data = response.data;
+      res.json(data);
+    } else {
+      res.status(response.status).json({ error: `Failed to fetch data. Status code: ${response.status}` });
+    }
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
 });
 
 app.use('/api/v1', api);
